feb2a9336eb17d4da35e87c4c27ae13eaa108e7e
krupalsinhchavda <krupalsinh.chavda.epistic@gmail.com>
2025-02-03 13:18:10 +0530

    all changes

--- src/app/components/projects/projects-routing.module.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProjectListComponent } from './project-list/project-list.component';
-
-export const ProjectsRoutingModule: Routes = [
-  { path: '', component: ProjectListComponent, pathMatch: 'full' },
-];
